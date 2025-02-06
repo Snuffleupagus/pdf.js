996c17f750a8840626f61b97a2b673e6c47e1366
Jonas Jenwald <jonas.jenwald@gmail.com>
2025-02-06 09:09:29 +0100

    [api-minor] Disable `eval` support by default
    The idea behind this patch is to see if disabling of `eval` leads to any reports about bad performance, since the previous patch should have improved things a fair bit.

--- web/app_options.js
+++ web/app_options.js
@@ -408,7 +408,7 @@ const defaultOptions = {
   },
   isEvalSupported: {
     /** @type {boolean} */
-    value: true,
+    value: false,
     kind: OptionKind.API,
   },
   isOffscreenCanvasSupported: {
